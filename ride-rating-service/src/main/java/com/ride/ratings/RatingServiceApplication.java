70cfe75f8e3a254c4d7a212ed000cb57b4e32160
Aman Kumar Singh <apkaman9@gmail.com>
2023-11-04 21:32:35 +0530

    Eureka Service Setup

--- ride-rating-service/src/main/java/com/ride/ratings/RatingServiceApplication.java
+++ ride-rating-service/src/main/java/com/ride/ratings/RatingServiceApplication.java
@@ -4,8 +4,10 @@ package com.ride.ratings;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RatingServiceApplication implements CommandLineRunner {
 
 
